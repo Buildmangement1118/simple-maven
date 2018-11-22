96837c8586f102cb0ccc7a44e1e2789f89419f64
W8Admin <W8Admin@raum05pc01>
2018-11-22 14:04:55 +0100

    Different source/target versions
    - Exclude jar plugin

--- src/main/java/de/integrata/buld/paux/DoIt.java
+++ src/main/java/de/integrata/buld/paux/DoIt.java
@@ -5,7 +5,6 @@ import java.util.Arrays;
 public class DoIt {
 
 	static int add(int left, int right) {
-		Arrays.asList(left, right).stream().anyMatch(x -> x > 0);
 		return left + right;
 	}
 	
--- src/test/java/de/integrata/buld/paux/DoItTest2.java
+++ src/test/java/de/integrata/buld/paux/DoItTest2.java
@@ -2,6 +2,8 @@ package de.integrata.buld.paux;
 
 import static org.junit.Assert.assertThat;
 
+import java.util.Arrays;
+
 import org.hamcrest.CoreMatchers;
 import org.junit.Test;
 
@@ -9,6 +11,8 @@ public class DoItTest2 {
 
 	@Test
 	public void testAdd() {
+		Arrays.asList(1,2).stream().anyMatch(x -> x > 0);
+
 		assertThat(DoIt.add(1, 2), CoreMatchers.is(3));
 	}
 	

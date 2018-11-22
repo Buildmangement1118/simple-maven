4e9ca9c32b9881f70699f5fddbef8a38a7f2b137
W8Admin <W8Admin@raum05pc01>
2018-11-22 11:59:17 +0100

    Make runnable

--- src/main/java/de/integrata/buld/paux/DoIt.java
+++ src/main/java/de/integrata/buld/paux/DoIt.java
@@ -4,15 +4,13 @@ import java.util.Arrays;
 
 public class DoIt {
 
-	
-	
-	
 	static int add(int left, int right) {
-		
 		Arrays.asList(left, right).stream().anyMatch(x -> x > 0);
-
-		
 		return left + right;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Erfolg");
 	}
 	
 	

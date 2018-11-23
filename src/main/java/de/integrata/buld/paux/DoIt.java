091dc51b0c208d6a8e59826909cacf1d8c94d13d
W8Admin <W8Admin@raum05pc01>
2018-11-23 10:58:23 +0100

    Maven BOM / import scope

--- src/main/java/de/integrata/buld/paux/DoIt.java
+++ src/main/java/de/integrata/buld/paux/DoIt.java
@@ -2,9 +2,14 @@ package de.integrata.buld.paux;
 
 import java.util.Arrays;
 
+import org.apache.commons.pool.ObjectPool;
+
 public class DoIt {
 
 	static int add(int left, int right) {
+		
+		ObjectPool pool = null;
+		
 		return left + right;
 	}
 	
